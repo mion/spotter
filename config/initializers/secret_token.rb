34b1f26fe4ae717276d07d32339f82347a43a67d
Gabriel Vieira <gluisvieira@gmail.com>
2013-06-08 17:06:47 -0300

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spotter::Application.config.secret_token = '76d81d8e8c65ab944e0122d07ffef2290ce539e04ea651b738fe5eedf267ee3b151fa842df66d82412dc216f573d779f4b1a244455d9475d87c9c6aa2ed2a49c'
