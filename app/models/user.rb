7a0c1647c7e88b867ce69c27ab9d19a6288187e8
Gabriel Vieira <gluisvieira@gmail.com>
2013-06-08 23:56:36 -0300

    Create database seed, it's sort of broken though.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,8 @@ class User < ActiveRecord::Base
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_uniqueness_of :email, case_sensitive: false
+
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
