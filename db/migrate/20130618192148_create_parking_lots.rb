0fdf1d23d711cab6fe7c49aa9bad3d4bfafb7869
Gabriel Vieira <gluisvieira@gmail.com>
2013-06-18 17:57:02 -0300

    Fix views and ParkingLot model.

--- db/migrate/20130618192148_create_parking_lots.rb
+++ db/migrate/20130618192148_create_parking_lots.rb
@@ -1,11 +1,11 @@
 class CreateParkingLots < ActiveRecord::Migration
   def self.up
     create_table :parking_lots do |t|
-      t.string :address
-      t.integer :spots_total
-      t.integer :spots_current
-      t.string :pricing
-      t.string :schedule
+      t.string :address, null: false
+      t.integer :spots_total, null: false
+      t.integer :spots_current, null:false, default: 0
+      t.string :pricing, null: false
+      t.string :schedule, null: false
       t.references :user
 
       t.timestamps
--- db/migrate/20130618200424_create_reservations.rb
+++ db/migrate/20130618200424_create_reservations.rb
@@ -1,10 +1,10 @@
 class CreateReservations < ActiveRecord::Migration
   def self.up
     create_table :reservations do |t|
-      t.string :credit_card
-      t.datetime :starts_at
-      t.datetime :ends_at
-      t.decimal :price
+      t.string :credit_card, null: false
+      t.datetime :starts_at, null: false
+      t.datetime :ends_at, null: false
+      t.decimal :price, null: false
       t.references :user
       t.references :parking_lot
 
